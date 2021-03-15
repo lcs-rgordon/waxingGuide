ff5f694b9f4ff3b7fe04edc84c090c7e7b023a75
Evan Armstrong <earmstrong@lcs.on.ca>
2021-03-15 12:08:55 -0400

    Initial Commit

---
+++ waxingGuide/waxingGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  waxingGuideApp.swift
+//  waxingGuide
+//
+//  Created by Evan Armstrong on 2021-03-15.
+//
+
+import SwiftUI
+
+@main
+struct waxingGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
