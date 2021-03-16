22264d652782daafee76303953fee4efe51249e5
Evan Armstrong <earmstrong@lcs.on.ca>
2021-03-16 15:02:38 -0400

    Started writing text for what sport to do.

---
+++ waxingGuide/Views/sports.swift
@@ -0,0 +1,24 @@
+//
+//  sports.swift
+//  waxingGuide
+//
+//  Created by Evan Armstrong on 2021-03-15.
+//
+
+import SwiftUI
+
+struct sports: View {
+    var body: some View {
+        VStack{
+            
+         Text("There are many different snow sports in this world that are all very fun to do.  A couple of these sports require waxing")
+            
+        }
+    }
+}
+
+struct sports_Previews: PreviewProvider {
+    static var previews: some View {
+        sports()
+    }
+}
