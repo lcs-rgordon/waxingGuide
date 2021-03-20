ded31095a361b8b8e16b78e01a43aa7e3bb2cf43
Evan Armstrong <earmstrong@lcs.on.ca>
2021-03-19 23:11:34 -0400

    Added more onto warm wax

--- waxingGuide/Views/waxRecomendationWithTool/warmWax.swift
+++ waxingGuide/Views/waxRecomendationWithTool/warmWax.swift
@@ -8,8 +8,19 @@
 import SwiftUI
 
 struct warmWax: View {
+   
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            Text("""
+These conditions can be some of the nicest to ski in but can sometimes be very slow (if it is above 0)  for these temperatures I would recomend the swix HS8 or the swix TS8B.
+""")
+            HStack{
+                Image("warmWax")
+                Image("warmWax2")
+
+                    
+            }
+        }
     }
 }
 
