00b85136597e1d4edd268f90bdca2caf922ea647
Evan Armstrong <earmstrong@lcs.on.ca>
2021-03-19 22:41:10 -0400

    Almost finished the final page for the warmest wax

--- waxingGuide/Views/waxRecomendationWithTool/warmestWax.swift
+++ waxingGuide/Views/waxRecomendationWithTool/warmestWax.swift
@@ -9,12 +9,22 @@ import SwiftUI
 
 struct warmestWax: View {
     var body: some View {
-        VStack{
-        Text("""
-Looks like you are planning on going on a pretty warm day
-
-
+        ScrollView{
+            Text("""
+Looks like you are planning on going on a pretty warm day.  These conditions are never really fast but the warm wax is the best you can do to help with this.
 """)
+                .padding(.horizontal, 12.0)
+            HStack{
+                Image("warmestWax2")
+                Image("warmestWax")
+            }
+            
+            Text("""
+Set your iron temp to 120 degrees celcius or 248 degrees fehrenheit.  Put wax on the ski than let cool for 10 minutes and scrape the wax off.  If you are going to be doing a lot of warm whether skiing I recommend getting a rilling tool.  (A tool that puts linear grooves in the bottom of your skis. These grooves break up suction when skiing on warm wet snow. In these conditions structure does more for ski speed than wax does. If you have a pattern stoneground in your skis you can get by without this tool.)
+""")
+                .padding(.horizontal, 12.0)
+            Spacer()
+            Image("rilingTool")
         }
     }
 }
