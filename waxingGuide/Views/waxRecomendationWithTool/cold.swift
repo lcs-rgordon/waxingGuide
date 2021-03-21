3876af7d181f6d68eb83b84eea02945e2222e1bf
Evan Armstrong <earmstrong@lcs.on.ca>
2021-03-20 20:52:04 -0400

    end of Saturday work

--- waxingGuide/Views/waxRecomendationWithTool/cold.swift
+++ waxingGuide/Views/waxRecomendationWithTool/cold.swift
@@ -10,7 +10,17 @@ import SwiftUI
 struct cold: View {
     var body: some View {
         ScrollView{
-            Text("For temperatures in between ")
+            Text("For temperatures in between -10 and -18 degrees celcius the best wax is the HS5 and TS5B.")
+                .padding(.horizontal, 15.0)
+            
+            HStack{
+                Image("cold")
+                    .resizable()
+                    .frame(width: 100, height: 250)
+                Image("cold2")
+            }
+            Text("Set the iron temperature to 160 degrees celcius( 320 degrees fahrenheit) to apply the wax.  Put wax on the ski than let cool for 10 minutes or more and scrape the wax off.")
+                .padding(.horizontal, 15.0)
         }
         
     }
--- waxingGuide/Views/waxRecomendationWithTool/freezing.swift
+++ waxingGuide/Views/waxRecomendationWithTool/freezing.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct freezing: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+        Text("For temperatures in between -14 and -32 degrees celcius the best wax is the pro polar.")
+        }
     }
 }
 
